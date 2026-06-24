7b16bbe61edaa69fa9a6b7084996dd68d10ab2e0
open-meadow <rohan.varughese200@gmail.com>
2026-06-24 14:21:44 +0530

    Load pdf page

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-def main():
-    print("Hello from rag-app!")
+import sys
+from ingest import load_pdf_pages
 
+def main(*args):
+    print("Hello from rag-app!")
+    print("You have passed: ", args)
+    print(f"Will pass {args[0][1]} to ingest")
+
+    file_path = args[0][1]
+    docs = load_pdf_pages(file_path)
+    
+    print("docs", docs)
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv)
