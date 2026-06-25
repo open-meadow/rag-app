94d944bacfdbfe275354c1f49a4101bdac79e8fe
open-meadow <rohan.varughese200@gmail.com>
2026-06-25 15:46:38 +0530

    Initialize frontend and add CORS

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from ingest import ingest
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ["*"],
+    allow_methods = ["*"],
+    allow_headers = ["*"]
+)
 
 @app.get("/ingest")
 def ingest_data():
@@ -10,6 +18,11 @@ def ingest_data():
     return {
         "message": "Data has been ingested"
     }
+
+@app.post("/query")
+async def user_query():
+    print("Backend received request")
+    return { "message": "2" }
 
 @app.get("/")
 async def root():
