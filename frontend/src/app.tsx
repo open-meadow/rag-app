49cbc675c20acbccedd159df51c60170d36f4dfb
open-meadow <rohan.varughese200@gmail.com>
2026-06-30 17:17:19 +0530

    Create login screen

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -6,33 +6,56 @@ const BACKEND_URL = "http://127.0.0.1:8001";
 const USERNAME = "johndoe";
 const PASSWORD = "secret";
 
-const Interface = () => {
+const LoginScreen = ({ handleLogin }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const getLoginToken = async (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+
+    const form = new FormData();
+    form.append("username", USERNAME);
+    form.append("password", PASSWORD);
+  
+    const res = await fetch(`${BACKEND_URL}/token`, {
+      method: "POST",
+      body: form
+    });
+  
+    const token = await res.json();
+
+    handleLogin(token);
+  }
+
+  return(
+    <>
+      <h3>Login Screen</h3>
+      <form onSubmit={getLoginToken}>
+        <label for="usernameInput">Username</label>
+        <input id="usernameInput" type='text' value={username} onInput={e => setUsername(e.currentTarget.value)}/>
+        <label for="passwordInput">Password</label>
+        <input id="passwordInput" type='password' value={password} onInput={e => setPassword(e.currentTarget.value)}/>
+        <button type='submit'>Login</button>
+      </form>
+    </>
+  );
+}
+
+const Interface = ({loginToken}) => {
   const [loadingMessage, setLoadingMessage] = useState("No operation is being conducted");
   const [llmThoughts, setLLMThoughts] = useState("");
   const [llmResponse, setLLMResponse] = useState("");
   const [text, setText] = useState("");
   const fileInputRef = useRef<HTMLInputElement>(null);
 
-  const getLoginToken = async () => {
-    const form = new FormData();
-    form.append("username", USERNAME);
-    form.append("password", PASSWORD);
-
-    const res = await fetch(`${BACKEND_URL}/token`, {
-      method: "POST",
-      body: form
-    });
-
-    return res.json();
-  }
 
   // When text is input and "Submit" button is clicked
   const handleChat = async (text: string) => {
     setLoadingMessage("Response is being retrieved from LLM");
     setText("");
     
-    const loginToken = await getLoginToken();
-    console.log("LoginToken: ", loginToken);
+    // const loginToken = await getLoginToken();
+    // console.log("LoginToken: ", loginToken);
 
     const res = await fetch(`${BACKEND_URL}/query`, {
       method: "POST",
@@ -54,7 +77,7 @@ const Interface = () => {
     const files = event.target.files;
     if(!files) return;
     
-    const loginToken = await getLoginToken();
+    // const loginToken = await getLoginToken();
     
     const file = files[0];
     const formData = new FormData();
@@ -84,7 +107,6 @@ const Interface = () => {
         <input id="inputBox" value={text} onInput={e => setText(e.currentTarget.value)}></input>
         <div id="buttons">
           <button onClick={() => handleChat(text)}>Send</button>
-          {/* <button onClick={() => handleIngest()}>Re-Ingest</button> */}
           <button onClick={handleButtonClick}>Ingest</button>
           <input ref={fileInputRef} type='file' hidden onChange={handleFileUpload}></input>
         </div>
@@ -107,10 +129,17 @@ const Interface = () => {
 }
 
 export function App() {
+  const [token, setToken] = useState(null);
+
+  const handleLogin = (tokenData) => {
+    setToken(tokenData);
+  }
+
   return (
     <>
       <h1>U-TO PeopleAssist</h1>
-      <Interface />
+      {!token && <LoginScreen handleLogin={handleLogin}/>}
+      {token && <Interface loginToken={token} />}
     </>
   )
 }
