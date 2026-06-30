465ca522aa2594eb0cfae23735026f3779171edd
open-meadow <rohan.varughese200@gmail.com>
2026-06-30 12:22:11 +0530

    Move security routes to auth.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from typing import Annotated
-from fastapi import FastAPI, File, UploadFile, Depends
+from fastapi import FastAPI, File, HTTPException, status, UploadFile, Depends
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from pathlib import Path
@@ -20,32 +20,6 @@ app.add_middleware(
 
 class Query(BaseModel):
     user_query: str
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-class User(BaseModel):
-    username: str
-    email: str | None = None
-    full_name: str | None = None
-    disabled: bool | None = None
-
-@app.get("/items/")
-async def read_items(token: Annotated[str, Depends(oauth2_scheme)]):
-    return { "token": token }
-
-def fake_decode_token(token):
-    return User(
-        username = token + "fakedecoded",
-        email="john@example.com",
-        full_name="John Doe"
-    )
-
-async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
-    user = fake_decode_token(token)
-    return user
-
-@app.get("/users/me")
-async def read_users_me(current_user: Annotated[User, Depends(get_current_user)]):
-    return current_user
 
 # Adds file to /data/unstructured, and re-ingests the data in that file
 @app.post("/ingest")
